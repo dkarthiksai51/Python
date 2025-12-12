2f9fc43bfc56e121a57a279231f0b253f3f1b182
dkarthiksai51 <dkarthiksai51@gmail.com>
2025-12-12 11:12:45 +0530

    Add files via upload
    code for Number of digits

---
+++ find number of digits.py
@@ -0,0 +1,11 @@
+
+# Write a program to find number of digits in a given number
+
+
+
+num=6574
+count=0 
+while num!=0:           
+    num=num//10         
+    count=count+1        
+print("Number of digits : ",count)
